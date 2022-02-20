dd061c345698e49fc456466c14beacc249c9729d
cielio <cielion@hotmail.com>
2022-02-20 18:53:02 -0300

    Adicionar arquivos de projeto.

---
+++ ExeciciosUML/Moldels/ContaLuz.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeciciosUML.Moldels
+{
+    public class ContaLuz
+    {
+        public ContaLuz(DateTime dataLeitura, int numeroLeitura, int qtdKwGasto)
+        {
+            DataLeitura = dataLeitura;
+            NumeroLeitura = numeroLeitura;
+            QtdKwGasto = qtdKwGasto;
+        }
+
+        public DateTime DataLeitura { get; private set; }
+        public int NumeroLeitura { get; private set; }
+        public int QtdKwGasto { get; private set; }
+        public double ValorPagar { get; set; }
+        public DateTime DataPagamento { get; set; }
+        public double MediaConsumo { get; set; }
+
+        public static ContaLuz VerificaMesMenorConsumo(List<ContaLuz> contas)
+        {
+            if (contas.Count <= 0)
+                throw new ArgumentException("A lista esta vazia");
+
+            return contas.OrderBy(conta => conta.QtdKwGasto).First();
+        }
+
+        public static ContaLuz VerificaMesMaiorConsumo(List<ContaLuz> contas)
+        {
+            if (contas.Count <= 0)
+                throw new ArgumentException("A lista esta vazia");
+  
+            return contas.OrderBy(conta => conta.QtdKwGasto).Last();
+        }
+
+        public double CalcMediaConsumo()
+        {
+            return QtdKwGasto / 30;
+        }
+
+        public double CalcValorPagar() 
+        {
+            return QtdKwGasto * 0.4487;
+        }
+
+        public override string ToString()
+        {
+            return $"Data Leitura: {DataLeitura.ToShortDateString()} NºLeitura: {NumeroLeitura} Kw gasto {QtdKwGasto} Valor: {ValorPagar:F2}";
+        }
+    }
+}
---
+++ ExeciciosUML/Program.cs
@@ -0,0 +1,37 @@
+﻿using ExeciciosUML.Moldels;
+using System;
+using System.Collections.Generic;
+
+namespace ExeciciosUML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contas = new List<ContaLuz>()
+            {
+                new ContaLuz(DateTime.Parse("10/01/2022"),4601,460),
+                new ContaLuz(DateTime.Parse("10/02/2022"),4602,350),
+                new ContaLuz(DateTime.Parse("10/03/2022"),4603,341),
+                new ContaLuz(DateTime.Parse("10/04/2022"),4604,654),
+            };
+
+            foreach (var conta in contas)
+            {
+                conta.ValorPagar = conta.CalcValorPagar();
+                conta.MediaConsumo = conta.CalcMediaConsumo();
+            }
+
+            foreach (var conta in contas)
+            {
+                Console.WriteLine(conta);
+            }
+
+            var menorConsumo = ContaLuz.VerificaMesMenorConsumo(contas);
+            var MaiooConsumo = ContaLuz.VerificaMesMaiorConsumo(contas);
+            Console.WriteLine();
+            Console.WriteLine($"Menor Consumo {menorConsumo.QtdKwGasto} {menorConsumo.DataLeitura.ToShortDateString()}");
+            Console.WriteLine($"Maior Consumo {MaiooConsumo.QtdKwGasto} {MaiooConsumo.DataLeitura.ToShortDateString()}");
+        }
+    }
+}
